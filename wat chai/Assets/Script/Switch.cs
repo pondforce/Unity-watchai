30bc4c2d2deb031609637629b726c2ba8cb15c92
pondforce <pond_529364016@hotmail.com>
2019-02-21 18:06:28 +0700

    wat chai model
    show model

---
+++ wat chai/Assets/Script/Switch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public GameObject Camera1;
+    public GameObject Camera2;
+    public GameObject Camera3;
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Z))
+        {
+            Camera1.active = true;
+            Camera2.active = false;
+            Camera3.active = false;
+
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            Camera1.active = false;
+            Camera2.active = true;
+            Camera3.active = false;
+
+        }
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            Camera1.active = false;
+            Camera2.active = false;
+            Camera3.active = true;
+
+        }
+    }
+
+
+
+}
